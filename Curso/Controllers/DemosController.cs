d82c88fb3043bf43fb5ef87a2b0d05660c4ed60d
Javier <jmartin@grupoloyal.com>
2021-06-21 13:26:27 +0200

    Modelos y controladores

---
+++ Curso/Controllers/DemosController.cs
@@ -0,0 +1,26 @@
+﻿using Curso.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso.Controllers {
+    public class DemosController : Controller {
+        public IActionResult Index() {
+            return this.Content("Hola mundo");
+        }
+        public IActionResult Saluda(string id) {
+            return this.Content("Hola " + id);
+        }
+
+        public IActionResult Despide() {
+            return this.Content("Adios mundo");
+        }
+        public IActionResult Contacto() {
+            var srv = new ContactoService();
+            var c = srv.Get(1);
+            return this.Content($"Hola {c.Nombre} {c.Apellidos}");
+        }
+    }
+}
---
+++ Curso/Models/Contacto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso.Models {
+    public class Contacto {
+        public int Id { get; set; }
+        public string Nombre  { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+
+        public bool Activo { get; set; } = true;
+
+        public void Desactivar() {
+            Activo = false;
+        }
+        public bool EsValido() {
+            return true;
+        }
+        public bool EsInvalido() {
+            return !EsValido();
+        }
+
+    }
+}
